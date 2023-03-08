0f2263aa10f4de9d87aa9b06cde48590c2c993af
anwooseong <aws5624@naver.com>
2023-03-08 15:48:47 +0900

    본인인증 로그인 API

--- src/main/java/com/example/demo/src/user/UserProvider.java
+++ src/main/java/com/example/demo/src/user/UserProvider.java
@@ -3,6 +3,7 @@ package com.example.demo.src.user;
 import com.example.demo.config.BaseException;
 import com.example.demo.src.user.model.*;
 import com.example.demo.utils.JwtService;
+import com.example.demo.utils.SHA256;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -48,6 +49,31 @@ public class UserProvider {
             return userDao.checkStoreName(randomStoreName);
         } catch (Exception exception) {
             throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    public PostLoginRes login(PostLoginReq postLoginReq) throws BaseException {
+
+        User user = userDao.getPwd(postLoginReq);
+        String encryptPwd;
+
+        try {
+            encryptPwd = new SHA256().encrypt(postLoginReq.getPassword());
+        } catch (Exception exception) {
+            throw new BaseException(PASSWORD_DECRYPTION_ERROR); // 4012, "비밀번호 복호화에 실패하였습니다."
+        }
+        if (userDao.checkLoginReportUser(postLoginReq, encryptPwd) == 1) {
+            throw new BaseException(POST_USERS_REPORT_USER);
+        }
+        if (userDao.checkUid(postLoginReq.getUid()) == 0) {
+            throw new BaseException(FAILED_TO_LOGIN); // 3014,"없는 아이디거나 비밀번호가 틀렸습니다."
+        }
+        if (user.getPassword().equals(encryptPwd)) {
+            int userId = user.getUserId();
+            String jwt = jwtService.createJwt(userId);
+            return new PostLoginRes(userId, user.getStoreName(), user.getName(), jwt);
+        } else {
+            throw new BaseException(FAILED_TO_LOGIN); // 3014,"없는 아이디거나 비밀번호가 틀렸습니다."
         }
     }
 }
