3ab7e50c006ba15cc92e6ebd2c84f2c19de2cd93
sy673 <sylee6529@gmail.com>
2023-03-11 23:50:35 +0900

    Feat: 회원 상점 후기 조회 API 구현

---
+++ src/main/java/com/example/demo/src/review/model/GetReviewRes.java
@@ -0,0 +1,23 @@
+package com.example.demo.src.review.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.exception.DataException;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetReviewRes {
+    private int startRating;
+    private String content;
+    private String userName;
+    private Date createdAt;
+    private int productId;
+    private String productTitle;
+    private String isSafePay;
+}
