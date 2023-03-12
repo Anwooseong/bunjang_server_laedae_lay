320c24e9ca7a6a1c151126f37721fe6549cec340
sy673 <sylee6529@gmail.com>
2023-03-12 17:14:03 +0900

    Feat: 상품 정보 조회 API 구현

--- src/main/java/com/example/demo/src/product/ProductDao.java
+++ src/main/java/com/example/demo/src/product/ProductDao.java
@@ -1,10 +1,8 @@
 package com.example.demo.src.product;
 
 import com.example.demo.src.product.dto.MainProductDto;
-import com.example.demo.src.product.model.GetMainProductRes;
-import com.example.demo.src.product.model.GetSearchProductRes;
-import com.example.demo.src.product.model.GetSimilarProductRes;
-import com.example.demo.src.product.model.PostProductReq;
+import com.example.demo.src.product.model.*;
+import com.example.demo.src.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -122,5 +120,73 @@ public class ProductDao {
         String query = "insert into Product_Tag(product_id, tag_id) VALUES (?,?)";
         Object[] params = new Object[]{lastInsertId, tagId};
         this.jdbcTemplate.update(query,params);
+    }
+
+    public List<String> getProductImgUrls(int productId) {
+        String getProductImgUrlsquery = "select url " +
+                "from Product P join ProductImg PI on P.id = PI.product_id " +
+                "where P.id = ?";
+        String getProductImgUrlsParam = String.valueOf(productId);
+        return this.jdbcTemplate.query(getProductImgUrlsquery,
+                (rs,rowNum)-> rs.getString("url")
+                , getProductImgUrlsParam);
+    }
+
+    public List<String> getTags(int productId) {
+        String getTagssquery = "select name " +
+                "from Product_Tag join Product P on P.id = Product_Tag.product_id " +
+                "join Tag T on Product_Tag.tag_id = T.id " +
+                "where product_id = ?";
+        String getTagsParam = String.valueOf(productId);
+        return this.jdbcTemplate.query(getTagssquery,
+                (rs,rowNum)-> rs.getString("url")
+                , getTagsParam);
+    }
+
+    public int getChatCounts(int productId) {
+        String getChatCountsquery = "select count(*) as \"chat_count\" " +
+                "from Product join ChatRoom CR on Product.id = CR.product_id " +
+                "where product_id = ?";
+        String getChatCountsParam = String.valueOf(productId);
+        return this.jdbcTemplate.queryForObject(getChatCountsquery, int.class, getChatCountsParam);
+    }
+
+    public int getLikes(int productId) {
+        String getLikesquery = "select count(*) as \"likes\" " +
+                "from Product join MyProduct MP on Product.id = MP.product_id " +
+                "where product_id = ?";
+        String getLikesParam = String.valueOf(productId);
+        return this.jdbcTemplate.queryForObject(getLikesquery, int.class, getLikesParam);
+    }
+
+    public GetProductDetailRes getProductInfo(int productId) {
+        String getProductInfoquery = "select price, is_safe_pay, Product.title, location_address, Product.created_at, view, has_delivery_fee, " +
+                "is_new, amount, is_interchangable, content, MC.img_url as \"category_img\", MC.title as \"category_title\", " +
+                "B.img_url as \"brand_img\", name as \"brand_name\" " +
+                "from Product " +
+                "join MajorCategory MC on Product.major_category_id = MC.id " +
+                "join Brand B on Product.brand_id = B.id " +
+                "where Product.id = ?";
+        String getProductInfoParam = String.valueOf(productId);
+
+        return this.jdbcTemplate.queryForObject(getProductInfoquery,
+                (rs, rowNum) -> new GetProductDetailRes(
+                        rs.getInt("price"),
+                        rs.getString("is_safe_pay"),
+                        rs.getString("title"),
+                        rs.getString("location_address"),
+                        rs.getString("created_at"),
+                        rs.getInt("view"),
+                        rs.getString("has_delivery_fee"),
+                        rs.getString("is_new"),
+                        rs.getInt("amount"),
+                        rs.getString("is_interchangable"),
+                        rs.getString("content"),
+                        rs.getString("category_img"),
+                        rs.getString("category_title"),
+                        rs.getString("brand_img"),
+                        rs.getString("brand_name")
+                ),
+                getProductInfoParam);
     }
 }
